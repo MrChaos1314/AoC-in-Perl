897e1464d9fb5271ae02599dd78f874a6fdccc4c
MrChaos1314 <mrchaos1213@gmail.com>
2024-12-13 17:29:03 +0100

    started Day9

---
+++ Day9/main.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use 5.10.0;
+use Data::Dumper;
+
+my $fh;
+my $fileLocation = $ARGV[0];
+
+if(!$fileLocation){
+    say("pls give a file!");
+    exit(1);
+}
+
+if(!(open($fh, "<", $fileLocation))){
+    say("WTF is going on - is this even a file???");
+    exit(1);
+}
+
+my @numbers;
+
+while(<$fh>){
+    chomp;
+    my $line = $_;
+    @numbers = split(//, $line);
+}
+
+say(Dumper(@numbers));
