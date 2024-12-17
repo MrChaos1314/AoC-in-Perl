50f3b5e1bd112296032afcb96bffd8bdce224321
MrChaos1314 <mrchaos1213@gmail.com>
2024-12-17 23:19:27 +0100

    solution for Day11

--- Day11/main.pl
+++ Day11/main.pl
@@ -4,6 +4,7 @@ use warnings;
 use strict;
 use 5.10.0;
 use Data::Dumper;
+use Storable qw(dclone);
 
 my $fh;
 my $fileLocation = $ARGV[0];
@@ -25,7 +26,7 @@ if(!(open($fh, "<", $fileLocation))){
 
 my @records;
 my %records_hash;
-keys %records_hash = 2000000;
+my %already_calced;
 
 while(<$fh>){
     chomp;
@@ -84,74 +85,118 @@ if($part == 2){
 #    say(Dumper(scalar @records));
 #}
 
-for(my $cycle = 0; $cycle < 6; $cycle++){
-    say($cycle);
-    say(Dumper(\%records_hash));
-    my %already_calced;
-    my @keys = keys %records_hash;
-    foreach my $num (@keys){
-        my $num_len = length($num);
-        if($records_hash{$num} != 0){
-            if($num == 0){
-                #this is stupid -> change -> look 3 smallData TODO
-                $records_hash{$num} = 1;
-            }elsif($num_len % 2 == 0){
-                if($already_calced{$num}){
-                    my @calc_info = split(/,/, $already_calced{$num});
-                    $records_hash{$num} = --$records_hash{$num};
-                    if($records_hash{$calc_info[0]}){
-                        my $value = $records_hash{$calc_info[0]};
-                        $records_hash{$calc_info[0]} = ++$value;
-                    }
-                    if($records_hash{$calc_info[1]}){
-                        my $value = $records_hash{$calc_info[1]};
-                        $records_hash{$calc_info[1]} = ++$value;
-                    }
-                }else{
-                    my @numbers = split(//,$num);
-                    my $numbers_half = $num_len / 2;
-                    my $left_nums;
-                    my $right_nums;
-                    for(my $left = 0; $left < $numbers_half; $left++){
-                        if($left_nums){
-                            $left_nums = $left_nums . $numbers[$left];
-                        }else{
-                            $left_nums = $numbers[$left];
-                        }
-                    }
-                    for(my $right = $numbers_half; $right < $num_len; $right++){
-                        if($right_nums){
-                            $right_nums = $right_nums . $numbers[$right];
-                        }else{
-                            $right_nums = $numbers[$right];
-                        }
-                    }
 
-                    $already_calced{$num} = "$left_nums,$right_nums";
-                    $records_hash{$num} = --$records_hash{$num};
-                    if($records_hash{$left_nums}){
-                        $records_hash{$left_nums} = ++$records_hash{$left_nums};
+for(my $cycle = 0; $cycle < $max; $cycle++){
+    say($cycle);
+    my %records_fake = %{dclone \%records_hash};
+    foreach my $num (keys %records_fake){
+        my $num_len = length($num);
+        if($records_fake{$num} != 0){
+                if($num == 0){
+                    if($records_hash{1}){
+                        my $value = $records_hash{1};
+                        $value = $value + 1 * $records_fake{$num};
+                        $records_hash{1} = $value;
                     }else{
-                        $records_hash{$left_nums} = 1;
+                        $records_hash{1} = 1;
+                        my $value = $records_hash{1};
+                        $value = $value + 1 * $records_fake{$num} - 1;
+                        $records_hash{1} = $value;
                     }
-                    if($records_hash{$right_nums}){
-                        $records_hash{$right_nums} = ++$records_hash{$right_nums};
+                    my $dec = $records_hash{$num};
+                    $dec = $dec - 1 * $records_fake{$num};
+                    $records_hash{$num} = $dec;
+                }elsif($num_len % 2 == 0){
+                    if($already_calced{$num}){
+                        my @calc_info = split(/,/, $already_calced{$num});
+                        my $left = $calc_info[0];
+                        my $right = $calc_info[1];
+                        if($records_hash{$left}){
+                            my $value = $records_hash{$left};
+                            $value = $value + 1 * $records_fake{$num};
+                            $records_hash{$left} = $value;
+                        }else{
+                            $records_hash{$left} = 1;
+                            my $value = $records_hash{$left};
+                            $value = $value + 1 * $records_fake{$num} - 1;
+                            $records_hash{$left} = $value;
+                        }
+                        if($records_hash{$right}){
+                            my $value = $records_hash{$right};
+                            $value = $value + 1 * $records_fake{$num};
+                            $records_hash{$right} = $value;
+                        }else{
+                            $records_hash{$right} = 1;
+                            my $value = $records_hash{$right};
+                            $value = $value + 1 * $records_fake{$num} - 1;
+                            $records_hash{$right} = $value;
+                        }
                     }else{
-                        $records_hash{$right_nums} = 1;
+                        my @numbers = split(//,$num);
+                        my $numbers_half = $num_len / 2;
+                        my $left_nums;
+                        my $right_nums;
+                        for(my $left = 0; $left < $numbers_half; $left++){
+                            if($left_nums){
+                                $left_nums = $left_nums . $numbers[$left];
+                            }else{
+                                $left_nums = $numbers[$left];
+                            }
+                        }
+                        for(my $right = $numbers_half; $right < $num_len; $right++){
+                            if($right_nums){
+                                $right_nums = $right_nums . $numbers[$right];
+                            }else{
+                                $right_nums = $numbers[$right];
+                            }
+                        }
+
+                        $already_calced{$num} = "$left_nums,$right_nums";
+                        if($records_hash{$left_nums}){
+                            my $value = $records_hash{$left_nums};
+                            $value = $value + 1 * $records_fake{$num};
+                            $records_hash{$left_nums} = $value;
+                        }else{
+                            $records_hash{$left_nums} = 1;
+                            my $value = $records_hash{$left_nums};
+                            $value = $value + 1 * $records_fake{$num} - 1;
+                            $records_hash{$left_nums} = $value;
+                        }
+                        if($records_hash{$right_nums}){
+                            my $value = $records_hash{$right_nums};
+                            $value = $value + 1 * $records_fake{$num};
+                            $records_hash{$right_nums} = $value;
+                        }else{
+                            $records_hash{$right_nums} = 1;
+                            my $value = $records_hash{$right_nums};
+                            $value = $value + 1 * $records_fake{$num} - 1;
+                            $records_hash{$right_nums} = $value;
+                        }
                     }
-                }
-            }else{
-                $records_hash{$num} = --$records_hash{$num};
-                if($records_hash{$num * 2024}){
-                    $records_hash{$num * 2024} = ++$records_hash{$num * 2024};
+                        my $dec = $records_hash{$num};
+                        $dec = $dec - 1 * $records_fake{$num};
+                        $records_hash{$num} = $dec;
                 }else{
-                    $records_hash{2024 * $num} = 1;
+                    if($records_hash{$num * 2024}){
+                        my $value = $records_hash{$num * 2024};
+                        $value = $value + 1 * $records_fake{$num};
+                        $records_hash{$num * 2024} = $value;
+                    }else{
+                        $records_hash{2024 * $num} = 1;
+                        my $value = $records_hash{$num * 2024};
+                        $value = $value + 1 * $records_fake{$num} - 1;
+                        $records_hash{$num * 2024} = $value;
+                        say($records_hash{$num});
+                    }
+                        my $dec = $records_hash{$num};
+                        $dec = $dec - 1 * $records_fake{$num};
+                        $records_hash{$num} = $dec;
                 }
-            }
         }
     }
 }
 
+print(Dumper(\%already_calced));
 
 my $result = 0;
 foreach my $key (keys %records_hash){
