27c74db77cd31bf63f54ee49bc1c57ae6a6121ec
MrChaos1314 <mrchaos1213@gmail.com>
2024-12-18 20:36:53 +0100

    Continue at todo

--- Day12/main.pl
+++ Day12/main.pl
@@ -26,26 +26,27 @@ while(<$fh>){
     push(@records, \@chars);
 }
 
-print(Dumper(\@records));
-
 my %occurence;
 
 for(my $row = 0; $row < scalar @records; $row++){
     for(my $column = 0; $column < scalar @{$records[$row]}; $column++){
-        if(!isInHash()){
-            consumeField();
+        my $char = ${$records[$row]}[$column];
+        if(!isInHash($row, $column, $char)){
+            consumeField($row, $column, $char);
         }
     }
 }
 
 sub isInHash{
-    my $cur_x = shift;
     my $cur_y = shift;
+    my $cur_x = shift;
+    my $char = shift;
     foreach my $key (%occurence){
         my @info = split(/,/, $key);
-        my $info_x = shift(@info);
         my $info_y = shift(@info);
-        if($info_x eq $cur_x && $info_y eq $cur_y){
+        my $info_x = shift(@info);
+        my $info_char = shift(@info);
+        if($info_x eq $cur_x && $info_y eq $cur_y && $info_char && $char){
             return 1;
         }
     }
@@ -53,18 +54,52 @@ sub isInHash{
 }
 
 sub consumeField{
-    my $cur_x = shift;
     my $cur_y = shift;
-    my $checker = 1;
-    #consume self
+    my $cur_x = shift;
+    my $char = shift;
 
-    my $value = "hier weitermachen";
-    $occurence{"$cur_x,$cur_y,${$records[$cur_x]}[$cur_y]"} = $value;
+    #consume self
+    $occurence{"$cur_y,$cur_x,$char"} = "yes";
+
     #check surrounding and consume it too
-    while($checker){
-        check_up($cur_x, $cur_y);
-        check_down($cur_x, $cur_y);
-        check_left($cur_x, $cur_y);
-        check_right($cur_x, $cur_y);
+    check_up($cur_y, $cur_x, $char);
+    check_down($cur_y, $cur_x, $char);
+    check_left($cur_y, $cur_x, $char);
+    check_right($cur_y, $cur_x, $char);
+}
+
+sub check_up{
+    my $cur_y = shift;
+    my $cur_x = shift;
+    my $char = shift;
+
+    my $new_y = $cur_y - 1;
+    my $new_x = $cur_x;
+
+    #TODO: think about what is needed and needs to be checked
+    if(${$records[$new_y]}[$new_x] eq $char){
+        if($occurence{"$new_y,$new_x,$char"}){
+
+        }else{
+            $occurence{"$new_y,$new_x,$char"} = "yes";
+        }
     }
+}
+
+sub check_down{
+    my $cur_y = shift;
+    my $cur_x = shift;
+
+}
+
+sub check_left{
+    my $cur_y = shift;
+    my $cur_x = shift;
+
+}
+
+sub check_right{
+    my $cur_y = shift;
+    my $cur_x = shift;
+
 }
